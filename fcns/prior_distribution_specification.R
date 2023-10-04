529a803d6ba3611bfcae1530f51efb40af0ed029
lucygf <101729754+1035825@users.noreply.github.com>
2023-10-04 11:01:28 +0100

    Add files via upload

--- fcns/prior_distribution_specification.R
+++ fcns/prior_distribution_specification.R
@@ -7,17 +7,17 @@
 # first use it as gamma to calculate. 
 
 limits <- c(1.6, 3)
-limits_tester <- limits-1
+limits_tester <- limits - 1
 
 r0_gamma_pars <- get.gamma.par(p = c(0.025, 0.975), q = limits_tester, 
                                show.output = F, plot = F)
+# r0_gamma_pars <- c(11.082591, 9.248767)
+# names(r0_gamma_pars) = c('shape', 'rate')
 
-# x <- c(1:100)
-# y <- rgamma(n=100, shape = r0_gamma_pars[1], rate = r0_gamma_pars[2])
-# #plot(x, y)
+# plot(dgamma(seq(0,4,0.01), shape = r0_gamma_pars[1], rate = r0_gamma_pars[2]), type='l')
+# r0_gamma_pars[1]/r0_gamma_pars[2] #mean
 
-
-# sucsceptibility, from Bens paper. 
+# susceptibility, from Bens paper. 
 # taking 95% to be lowest and median value in 2+ year olds (0.19, 0.5) <- immunity
 # taking mean of other medians as our median = 0.3925
 limits <- c(0.5,0.3925,0.19)
@@ -28,7 +28,23 @@ sus_beta_pars <- get.beta.par(p = c(0.025, 0.5, 0.975),
                               show.output = F, 
                               plot = F)
 
+# sus_beta_pars <- c(50.19925, 32.55043)
+# names(sus_beta_pars) <- c('shape1', 'shape2')
+
 #  x <- c(1:1000)
 #  y <- rbeta(n=1000, shape1 = sus_beta_pars[1], shape2 = sus_beta_pars[2])
 #  plot(x, y)
 # hist(y, breaks=20)
+
+# plot(dbeta(seq(0,1,0.01), shape1 = sus_beta_pars[1], shape2 = sus_beta_pars[2]), type='l')
+# sus_beta_pars[1]/(sus_beta_pars[1] + sus_beta_pars[2]) #mean
+
+
+
+
+
+
+  
+  
+  
+
