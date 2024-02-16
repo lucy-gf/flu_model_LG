6f5830c4a32f9a2ffc5920db41d04054c69b13c7
lucygf <101729754+1035825@users.noreply.github.com>
2024-02-16 10:43:42 +0000

    Update BS_epid_simulations.R

--- BS/BS_epid_simulations.R
+++ BS/BS_epid_simulations.R
@@ -14,8 +14,7 @@ years <- 30
 ## MAKE SURE THAT THE ORDER OF LOOPS ENSURES THE LEAST COMPUTATIONAL TIME
 
 # loops through each ITZ and vaccination program:
-#for(c_number in 1:7){
-for(c_number in c(6)){ 
+for(c_number in c(4)){ #1:7){
     c_name <- c("Africa", "Asia-Europe", "Eastern and Southern Asia",
                 "Europe", "Northern America", "Oceania-Melanesia-Polynesia",
                 "Southern America")[c_number]
@@ -34,6 +33,8 @@ for(c_number in c(6)){
     n_weeks <- length(seq.Date(from = as.Date(paste0(monday_start, "-01-", start_year), '%d-%m-%Y'),
                                to = as.Date(paste0("31-12-", (start_year + years - 1)), '%d-%m-%Y'),
                                by = 7))
+    
+    cases_rds <- list()
     
     for(vacc_prog_num in 1:length(vaccine_programs)){
       
@@ -62,8 +63,10 @@ for(c_number in c(6)){
         print(paste0(countrycode(country_code_index, origin='iso3c', destination = 'country.name')))
         print(Sys.time() - country_time)
       }
-      if(sims %% 20 == 0){
-        write_csv(cases_30_100, file = paste0("data/vacc_output/vacc_", cluster_code, "_vp_", vacc_prog_num, ".csv"))
+      if(sims %% 50 == 0){
+        cases_rds[[vacc_prog_num]] <- cases_30_100
+        saveRDS(cases_rds, file = paste0("data/vacc_output/vacc_", cluster_code, "_waning_changed_high_cov.rds"))
+        # write_csv(cases_30_100, file = paste0("data/vacc_output/vacc_", cluster_code, "_vp_", vacc_prog_num, ".csv"))
       }
       print(paste0('Vaccination program: ', vacc_prog_num,
                    ', Simulations complete: ', sims,
@@ -71,10 +74,6 @@ for(c_number in c(6)){
     }
   }
 }
-
-
-
-
 
 
 # ## ONLY RUNNING ONE COUNTRY
