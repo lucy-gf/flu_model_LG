d5f7fe1a7cf8f31ae1c4f9ff7c4704927e19e7fc
lucygf <101729754+1035825@users.noreply.github.com>
2024-02-16 10:44:03 +0000

    Update BS_vaccine_programs.R

--- BS/BS_vaccine_programs.R
+++ BS/BS_vaccine_programs.R
@@ -19,20 +19,80 @@ vacc_type_list <- list(
   ),
   improved_efficacy = list(
     imm_duration = 2,
-    coverage_pattern = 2,
+    coverage_pattern = 1,
     VE = c(0.9, 0.7, 0.7, 0.4),
     mismatch = T
   ),
   improved_breadth = list(
     imm_duration = 3,
-    coverage_pattern = 3,
+    coverage_pattern = 1,
     VE = c(0.7, 0.46, 0.7, 0.46),
     mismatch = F
   ),
   universal = list(
     imm_duration = 5,
-    coverage_pattern = 5,
+    coverage_pattern = 1,
     VE = c(0.9, 0.7, 0.9, 0.7),
+    mismatch = F
+  ),
+  current2 = list(
+    imm_duration = 1,
+    coverage_pattern = 1,
+    VE = c(0.7, 0.46, 0.42, 0.28), # c(Matched u65, 65+, mismatched u65, 65+)
+    mismatch = T
+  ),
+  improved_minimal2 = list(
+    imm_duration = 1,
+    coverage_pattern = 1,
+    VE = c(0.7, 0.46, 0.42, 0.28),
+    mismatch = T
+  ),
+  improved_efficacy2 = list(
+    imm_duration = 1,
+    coverage_pattern = 1,
+    VE = c(0.9, 0.7, 0.7, 0.4),
+    mismatch = T
+  ),
+  improved_breadth2 = list(
+    imm_duration = 1,
+    coverage_pattern = 1,
+    VE = c(0.7, 0.46, 0.7, 0.46),
+    mismatch = F
+  ),
+  universal2 = list(
+    imm_duration = 1,
+    coverage_pattern = 1,
+    VE = c(0.9, 0.7, 0.9, 0.7),
+    mismatch = F
+  ),
+  current3 = list(
+    imm_duration = 0.5,
+    coverage_pattern = 1,
+    VE = c(0.7, 0.46, 0.42, 0.28), # c(Matched u65, 65+, mismatched u65, 65+)
+    mismatch = T
+  ),
+  improved_minimal3 = list(
+    imm_duration = 1,
+    coverage_pattern = 1,
+    VE = c(0.7, 0.46, 0.42, 0.28),
+    mismatch = T
+  ),
+  improved_efficacy3 = list(
+    imm_duration = 2,
+    coverage_pattern = 1,
+    VE = c(0.7, 0.46, 0.42, 0.28),
+    mismatch = T
+  ),
+  improved_breadth3 = list(
+    imm_duration = 3,
+    coverage_pattern = 1,
+    VE = c(0.7, 0.46, 0.42, 0.28),
+    mismatch = F
+  ),
+  universal3 = list(
+    imm_duration = 5,
+    coverage_pattern = 1,
+    VE = c(0.7, 0.46, 0.42, 0.28),
     mismatch = F
   )
 )
@@ -47,7 +107,8 @@ default_v <- list(
   relative_vacc_infec = 1
 )
 
-coverage_vec <- c(0.6, 0.4, 0.0, 0.6)
+#coverage_vec <- c(0.5, 0.5, 0.0, 0.5)
+coverage_vec <- c(0.6, 0.6, 0, 0.6)
 
 # vaccine programs, list unfinished:
 vaccine_programs <- list(
@@ -91,6 +152,86 @@ vaccine_programs <- list(
             SH_vacc_date = default_v$SH_vacc_date,
             init_vaccinated = default_v$init_vaccinated,
             relative_vacc_infec = default_v$relative_vacc_infec)
+  # V12 = list(pop_coverage = coverage_vec,
+  #           vacc_type = 'current2',
+  #           weeks_vaccinating = default_v$weeks_vaccinating,
+  #           first_year_all = default_v$first_year_all,
+  #           NH_vacc_date = default_v$NH_vacc_date,
+  #           SH_vacc_date = default_v$SH_vacc_date,
+  #           init_vaccinated = default_v$init_vaccinated,
+  #           relative_vacc_infec = default_v$relative_vacc_infec),
+  # V22 = list(pop_coverage = coverage_vec,
+  #           vacc_type = 'improved_minimal2',
+  #           weeks_vaccinating = default_v$weeks_vaccinating,
+  #           first_year_all = default_v$first_year_all,
+  #           NH_vacc_date = default_v$NH_vacc_date,
+  #           SH_vacc_date = default_v$SH_vacc_date,
+  #           init_vaccinated = default_v$init_vaccinated,
+  #           relative_vacc_infec = default_v$relative_vacc_infec),
+  # V32 = list(pop_coverage = coverage_vec,
+  #           vacc_type = 'improved_efficacy2',
+  #           weeks_vaccinating = default_v$weeks_vaccinating,
+  #           first_year_all = default_v$first_year_all,
+  #           NH_vacc_date = default_v$NH_vacc_date,
+  #           SH_vacc_date = default_v$SH_vacc_date,
+  #           init_vaccinated = default_v$init_vaccinated,
+  #           relative_vacc_infec = default_v$relative_vacc_infec),
+  # V42 = list(pop_coverage = coverage_vec,
+  #           vacc_type = 'improved_breadth2',
+  #           weeks_vaccinating = default_v$weeks_vaccinating,
+  #           first_year_all = default_v$first_year_all,
+  #           NH_vacc_date = default_v$NH_vacc_date,
+  #           SH_vacc_date = default_v$SH_vacc_date,
+  #           init_vaccinated = default_v$init_vaccinated,
+  #           relative_vacc_infec = default_v$relative_vacc_infec),
+  # V52 = list(pop_coverage = coverage_vec,
+  #           vacc_type = 'universal2',
+  #           weeks_vaccinating = default_v$weeks_vaccinating,
+  #           first_year_all = default_v$first_year_all,
+  #           NH_vacc_date = default_v$NH_vacc_date,
+  #           SH_vacc_date = default_v$SH_vacc_date,
+  #           init_vaccinated = default_v$init_vaccinated,
+  #           relative_vacc_infec = default_v$relative_vacc_infec),
+  # V13 = list(pop_coverage = coverage_vec,
+  #           vacc_type = 'current3',
+  #           weeks_vaccinating = default_v$weeks_vaccinating,
+  #           first_year_all = default_v$first_year_all,
+  #           NH_vacc_date = default_v$NH_vacc_date,
+  #           SH_vacc_date = default_v$SH_vacc_date,
+  #           init_vaccinated = default_v$init_vaccinated,
+  #           relative_vacc_infec = default_v$relative_vacc_infec),
+  # V23 = list(pop_coverage = coverage_vec,
+  #           vacc_type = 'improved_minimal3',
+  #           weeks_vaccinating = default_v$weeks_vaccinating,
+  #           first_year_all = default_v$first_year_all,
+  #           NH_vacc_date = default_v$NH_vacc_date,
+  #           SH_vacc_date = default_v$SH_vacc_date,
+  #           init_vaccinated = default_v$init_vaccinated,
+  #           relative_vacc_infec = default_v$relative_vacc_infec),
+  # V33 = list(pop_coverage = coverage_vec,
+  #           vacc_type = 'improved_efficacy3',
+  #           weeks_vaccinating = default_v$weeks_vaccinating,
+  #           first_year_all = default_v$first_year_all,
+  #           NH_vacc_date = default_v$NH_vacc_date,
+  #           SH_vacc_date = default_v$SH_vacc_date,
+  #           init_vaccinated = default_v$init_vaccinated,
+  #           relative_vacc_infec = default_v$relative_vacc_infec),
+  # V43 = list(pop_coverage = coverage_vec,
+  #           vacc_type = 'improved_breadth3',
+  #           weeks_vaccinating = default_v$weeks_vaccinating,
+  #           first_year_all = default_v$first_year_all,
+  #           NH_vacc_date = default_v$NH_vacc_date,
+  #           SH_vacc_date = default_v$SH_vacc_date,
+  #           init_vaccinated = default_v$init_vaccinated,
+  #           relative_vacc_infec = default_v$relative_vacc_infec),
+  # V53 = list(pop_coverage = coverage_vec,
+  #           vacc_type = 'universal3',
+  #           weeks_vaccinating = default_v$weeks_vaccinating,
+  #           first_year_all = default_v$first_year_all,
+  #           NH_vacc_date = default_v$NH_vacc_date,
+  #           SH_vacc_date = default_v$SH_vacc_date,
+  #           init_vaccinated = default_v$init_vaccinated,
+  #           relative_vacc_infec = default_v$relative_vacc_infec)
 )
 
 
